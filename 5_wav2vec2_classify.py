43a612937ac60d64c3c0459f31c26f72cda900fb
Monica Romero <74182452+monirome@users.noreply.github.com>
2024-09-22 17:51:47 +0200

    Update 5_wav2vec2_classify.py

--- 5_wav2vec2_classify.py
+++ 5_wav2vec2_classify.py
@@ -6,7 +6,7 @@ import numpy as np
 import lightgbm as lgb
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
-from imblearn.over_sampling import SMOTE  # para balancear
+from imblearn.over_sampling import SMOTE 
 
 path = '/home/u917/PROJECT/aphasia/classify_codes/'
 
@@ -31,7 +31,7 @@ test_df['name_chunk_audio_path'] = test_df['name_chunk_audio_path'].str.replace(
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h", output_hidden_states=True)
 
-# Función para extraer la capa 6 de Wav2Vec 2.0
+# Funcion para extraer la capa 6 de Wav2Vec 2.0
 def extract_wav2vec2_layer(audio_path, layer_num=6):
     try:
         # Cargar audio y procesarlo con wav2vec2
@@ -50,7 +50,7 @@ def extract_wav2vec2_layer(audio_path, layer_num=6):
         print(f"Error al procesar el archivo {audio_path}: {e}")
         return np.zeros(768)  # Devolver vector de ceros si hay un error (dimensión de capa Wav2Vec2)
 
-# Extraer características con la capa 6 de Wav2Vec 2.0
+# Extraer caracteristicas con la capa 6 de Wav2Vec 2.0
 train_features = []
 test_features = []
 
@@ -60,7 +60,7 @@ for path in train_df['name_chunk_audio_path']:
 for path in test_df['name_chunk_audio_path']:
     test_features.append(extract_wav2vec2_layer(path, layer_num=6))  # Extraer la capa 6
 
-# Convertir las listas de características en DataFrames
+# Convertir las listas de caracteristicas en DataFrames
 train_features_df = pd.DataFrame(train_features)
 test_features_df = pd.DataFrame(test_features)
 
@@ -70,12 +70,12 @@ cols_to_drop = ['CIP', 'Marca', 'Transcrip_name', 'name_chunk_audio', 'balance_g
 train_df = train_df.drop(columns=cols_to_drop, errors='ignore')
 test_df = test_df.drop(columns=cols_to_drop, errors='ignore')
 
-# Convertir la variable objetivo (Fluente/No Fluente) a formato numérico
+# Convertir la variable objetivo (Fluente/No Fluente) a formato numerico
 le = LabelEncoder()
 train_df['Fluente/No Fluente'] = le.fit_transform(train_df['Fluente/No Fluente'])
 test_df['Fluente/No Fluente'] = le.transform(test_df['Fluente/No Fluente'])
 
-# Concatenar las características extraídas de los audios con el resto del dataset
+# Concatenar las caracteristicas extraidas de los audios con el resto del dataset
 X_train = pd.concat([train_df.drop('Fluente/No Fluente', axis=1), train_features_df], axis=1)
 y_train = train_df['Fluente/No Fluente']
 
@@ -90,7 +90,7 @@ X_test.columns = X_test.columns.astype(str)
 sm = SMOTE(sampling_strategy='auto', random_state=42)
 X_train_res, y_train_res = sm.fit_resample(X_train, y_train)
 
-# Parámetros modificados para LightGBM
+# Parametros modificados para LightGBM
 params = {
     'objective': 'binary',
     'metric': 'binary_logloss',
@@ -111,14 +111,14 @@ callbacks = [lgb.early_stopping(stopping_rounds=20)]  # aumentar el numero de it
 
 bst = lgb.train(params, train_data, num_round, valid_sets=[test_data], callbacks=callbacks)
 
-# Evaluación del modelo
+# Evaluacion del modelo
 y_pred = bst.predict(X_test)
 y_pred_binary = [1 if pred >= 0.5 else 0 for pred in y_pred]
 
-# Imprimir el reporte de clasificación
+# Imprimir el reporte de clasificacion
 print("Reporte de Clasificación:")
 print(classification_report(y_test, y_pred_binary, target_names=['No Fluente', 'Fluente']))
 
-# Imprimir la matriz de confusión
+# Imprimir la matriz de confusion
 print("Matriz de Confusión:")
 print(confusion_matrix(y_test, y_pred_binary))
